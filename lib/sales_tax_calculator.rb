0bc189793b1df8ffb8f32be820c3b04fdb5944de
Jorge Junior <jorgedjr21@gmail.com>
2025-01-24 16:09:21 -0300

    refactor file parser and add run method for sales tax calculator

--- lib/sales_tax_calculator.rb
+++ lib/sales_tax_calculator.rb
@@ -3,7 +3,13 @@
 require_relative "sales_tax_calculator/version"
 require_relative "sales_tax_calculator/file_parser"
 
+# SalesTaxCalculator module provides functionality to parse an input file
+# and calculate sales tax
 module SalesTaxCalculator
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.run(file_path)
+    parser = FileParser.new(file_path: file_path)
+    puts parser.parse_file
+  end
 end
