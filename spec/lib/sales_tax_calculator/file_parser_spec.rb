d50e98b0e7f6cc2554fd9a94226584b6983582e3
Jorge Junior <jorgedjr21@gmail.com>
2025-01-24 15:42:48 -0300

    adds fixtures and change rubocop  a rule for specs

--- spec/lib/sales_tax_calculator/file_parser_spec.rb
+++ spec/lib/sales_tax_calculator/file_parser_spec.rb
@@ -18,47 +18,47 @@ RSpec.describe SalesTaxCalculator::FileParser do
       end
     end
 
-    context 'with invalid file content' do
-      let(:file_path) { 'spec/fixtures/invalid_items.txt' }
+    context "with invalid file content" do
+      let(:file_path) { "spec/fixtures/invalid_items.txt" }
 
-      it 'returns an empty array' do
+      it "returns an empty array" do
         items = parser.parse_file
         expect(items).to eq([])
       end
     end
 
-    context 'with empty file' do
-      let(:file_path) { 'spec/fixtures/empty_items.txt' }
+    context "with empty file" do
+      let(:file_path) { "spec/fixtures/empty_items.txt" }
 
-      it 'returns an empty array' do
+      it "returns an empty array" do
         items = parser.parse_file
         expect(items).to eq([])
       end
     end
 
-    context 'with mixed valid and invalid lines' do
-      let(:file_path) { 'spec/fixtures/mixed_items.txt' }
+    context "with mixed valid and invalid lines" do
+      let(:file_path) { "spec/fixtures/mixed_items.txt" }
 
-      it 'parses only the valid lines and returns an array of items' do
+      it "parses only the valid lines and returns an array of items" do
         items = parser.parse_file
         expect(items).to eq([
-          { quantity: 1, item_name: 'book', price: 12.49 },
-          { quantity: 2, item_name: 'music CD', price: 14.99 }
-        ])
+                              { quantity: 1, item_name: "book", price: 12.49 },
+                              { quantity: 2, item_name: "music CD", price: 14.99 }
+                            ])
       end
     end
 
-    context 'with multiple valid formats' do
-      let(:file_path) { 'spec/fixtures/multiple_valid_items.txt' }
+    context "with multiple valid formats" do
+      let(:file_path) { "spec/fixtures/multiple_valid_items.txt" }
 
-      it 'parses the file and returns an array of items' do
+      it "parses the file and returns an array of items" do
         items = parser.parse_file
         expect(items).to eq([
-          { quantity: 3, item_name: 'chocolate bar', price: 0.85 },
-          { quantity: 1, item_name: 'packet of headache pills', price: 9.75 },
-          { quantity: 1, item_name: "book", price: 12.49 },
-          { quantity: 2, item_name: "music CD", price: 14.99 }
-        ])
+                              { quantity: 3, item_name: "chocolate bar", price: 0.85 },
+                              { quantity: 1, item_name: "packet of headache pills", price: 9.75 },
+                              { quantity: 1, item_name: "book", price: 12.49 },
+                              { quantity: 2, item_name: "music CD", price: 14.99 }
+                            ])
       end
     end
   end
