b4dd0ce723124054679598e94f50fb71621287d5
Jorge Junior <jorgedjr21@gmail.com>
2025-01-25 16:55:43 -0300

    create methods do calculate and print receipt

--- lib/sales_tax_calculator/receipt.rb
+++ lib/sales_tax_calculator/receipt.rb
@@ -8,6 +8,27 @@ module SalesTaxCalculator
       @items = items
       @sales_taxes = 0
       @total = 0
+      calculate_totals
+    end
+
+    def calculate_totals
+      items.each do |item|
+        tax = TaxCalculator.calculate(item)
+        total_price = item.price + tax
+        @sales_taxes += tax * item.quantity
+        @total += total_price * item.quantity
+      end
+    end
+
+    def print
+      items.each do |item|
+        tax = TaxCalculator.calculate(item)
+        total_price = item.price + tax
+        puts "#{item.quantity} #{item.name}: #{format("%.2f", (total_price * item.quantity))}"
+      end
+
+      puts "Sales Taxes: #{"%.2f" % sales_taxes}"
+      puts "Total: #{"%.2f" % total}"
     end
   end
 end
