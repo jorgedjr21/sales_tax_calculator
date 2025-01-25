a6b2d004a235d3cbd55a32caaacb48af13439d2d
Jorge David C.T Junior <jorgedjr21@gmail.com>
2025-01-25 15:09:47 -0300

    Create the file parser (#1)
    * adds all specs for the file parser
    
    * adds fixtures and change rubocop  a rule for specs
    
    * refactor file parser and add run method for sales tax calculator

--- lib/sales_tax_calculator/file_parser.rb
+++ lib/sales_tax_calculator/file_parser.rb
@@ -12,7 +12,15 @@ module SalesTaxCalculator
     def parse_file
       items = []
       File.foreach(file_path) do |line|
-        items << line
+        # https://rubular.com/r/q66OaTRkKDl2yB
+        match_data = line.match(/(\d+)\s(.+)\sat\s(\d+\.\d+)/)
+        next unless match_data
+
+        quantity = match_data[1].to_i
+        item_name = match_data[2]
+        price = match_data[3].to_f
+
+        items << { quantity: quantity, item_name: item_name, price: price }
       end
       items
     end
